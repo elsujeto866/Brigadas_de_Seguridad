18c916ea9403f4a23b157267761335da831facdc
Mixel12 <mario.vela@epn.edu.ec>
2024-01-10 13:40:14 -0500

    tercera actualizacion

--- src/components/ContenedorGeneral.jsx
+++ src/components/ContenedorGeneral.jsx
@@ -94,7 +94,7 @@ const Drawer = styled(MuiDrawer, {
   }),
 }));
 
-export default function ContenedorGeneral({ titulo }) {
+export default function ContenedorGeneral({ titulo, contenido }) {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
@@ -204,8 +204,7 @@ export default function ContenedorGeneral({ titulo }) {
             bottom: 0,
             left: 0,
             right: 0,
-            p: 2,
-            
+            p: 2,   
           }}
         >
           <img
@@ -216,39 +215,7 @@ export default function ContenedorGeneral({ titulo }) {
         </Box>
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-        <Box sx={{ display: "flex", justifyContent: "center", my: 2 }}>
-          <BotonGeneral texto="Ingresar" />
-        </Box>
+        {contenido}
       </Box>
     </Box>
   );

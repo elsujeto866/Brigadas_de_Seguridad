2298a9174f2d2b93d8fc87cc15aeba9f111b2b99
Elsujeto <71847792+elsujeto866@users.noreply.github.com>
2024-03-02 15:32:13 -0600

    Add bcryptjs and jsonwebtoken dependencies, and update auth.controller.js

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,20 +1,38 @@
 import Admin from "../models/admin.model.js";
-export const register = async (req, res) => {
-    const {email, password, name} = req.body;
+import bcrypt from "bcryptjs";
+import { createToken } from "../libs/jwt.js";
 
-    try {
-        const newAdmin = new Admin({
-            email,
-            password,
-            name,
-        });
-    
-        const adminSaved = await newAdmin.save()
-        res.send(adminSaved);
-        res.send("Administrador registrado exitosamente");
-    } catch (error) {
-        console.log(error);
-    }
-    
+export const register = async (req, res) => {
+  const { email, password, name } = req.body;
+
+  try {
+    //encriptar la contraseña
+    const passwordHash = await bcrypt.hash(password, 10);
+    const newAdmin = new Admin({
+      email,
+      name,
+      password: passwordHash,
+    });
+
+    const adminSaved = await newAdmin.save();
+    //crear token
+    const token = await createToken({
+      id: adminSaved._id,
+    });
+
+    res.cookie("token", token);
+    /*res.json({
+      message: "Admin created successfully",
+    });*/
+
+    //respuesta del servidor con un objeto json
+    res.json({
+      _id: adminSaved._id,
+      name: adminSaved.name,
+      email: adminSaved.email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 export const login = (req, res) => res.send("login");

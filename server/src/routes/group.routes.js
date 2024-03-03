fbe594161907b7cd1db4332e0c9889eebd3af477
Elsujeto <71847792+elsujeto866@users.noreply.github.com>
2024-03-03 14:51:38 -0600

    Error Grupos Arreglados

--- server/src/routes/group.routes.js
+++ server/src/routes/group.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import {getGroups, getGroup, createGroup, updateGroup, deleteGroup} from "../controllers/group.controller.js";
+import {getGroups, getGroupById, createGroup, updateGroup, deleteGroup} from "../controllers/group.controller.js";
 import {authRequired} from "../middlewares/validateToken.js";
 
 
 const router = Router();
 
 router.get('/', getGroups);
-router.get('/:id', getGroup);
+router.get('/:id', getGroupById);
 router.post('/new', createGroup);
 router.put('/:id', updateGroup);
 router.delete('/:id', deleteGroup);

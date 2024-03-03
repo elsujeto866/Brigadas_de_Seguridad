8efc4e3bbb126f428761c17521169fa255dfa7f7
Miguel Pastuña <david.rmig@gmail.com>
2024-03-03 10:14:24 -0500

    Correción, no se recibia el evento onClick como props

--- client/src/components/BotonGeneral.jsx
+++ client/src/components/BotonGeneral.jsx
@@ -1,7 +1,7 @@
 import Button from "@mui/material/Button";
 import "./styles/botonGeneral.css";
 
-export default function BotonGeneral({texto}) {
+export default function BotonGeneral({texto, onClick }) {
   return(
     <>
         <Button className = "boton-general" variant="contained" onClick={onClick}>{texto}</Button>
